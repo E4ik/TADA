0c0d1fd1a415d3a74032858e3dcd0ab02d99ddad
Erik <jjjay5223758@gmail.com>
2021-11-08 18:28:55 +0800

    Add User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get:"/about", to: "pages#index"
+  
+  get:"/sign_up", to:"users#sign_up"
 end
