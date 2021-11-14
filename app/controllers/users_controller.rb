12a7129c0f99b88592c3755433bed009ce0a7d52
Erik <jjjay5223758@gmail.com>
2021-11-14 23:16:58 +0800

    sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ActionController::Base
     def sign_up
+        @user = User.new
+    end
+
+    def account_verify
+    
+        clean_params = params.require(:user).permit(:username, :password, :mail)
+        @user = User.new(clean_params)
+
+
+        if @user.save
+            redirect_to "/"
+        else
+            render :"sign_up"
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get:"/about", to: "pages#index"
   
   get:"/sign_up", to:"users#sign_up"
+
+  post:"/account_verify", to:"users#account_verify"
 end
